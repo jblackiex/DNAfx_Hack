d66d43de94e8c04c18a87d9b073ae8187e0df33c
Niccolò Bordoni <bordoni@het.it>
2025-03-22 18:33:22 +0100

    keyboard interrupt handle

--- server/src/dnafx.py
+++ server/src/dnafx.py
@@ -6,6 +6,8 @@ from channel_manager import ChannelManager
 
 async def main():
     try:
+        # keyboard hook to close the program
+
         ENV.init_config()
         IOhub = ChannelManager()
         await asyncio.gather(IOhub.receive_socket(), IOhub.receive_keyboard(), IOhub.send_usbhid(), IOhub.send_gpio())
@@ -15,6 +17,9 @@ async def main():
 if __name__ == "__main__":
     try:
         asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received. Exiting.")
+        exit(0)
     except Exception as e:
         error_str = f"Error: {str(e)}" 
         print(error_str)
